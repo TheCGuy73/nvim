84cd682ad7eb1cb5af4b2dcbda14a27bd058a5c3
TheCGuy73 <112737430+TheCGuy73@users.noreply.github.com>
2025-05-07 17:33:29 +0200

    Delete nvim directory

--- nvim/init.lua
+++
@@ -1,3 +0,0 @@
-require "core.lazy"
-require "core.custom.general.keymaps"
-require "core.custom.general.options"
--- nvim/lua/core/custom/general/options.lua
+++
@@ -1,7 +0,0 @@
-vim.wo.number = true
-vim.opt.guicursor = "a:block"
-vim.opt.termguicolors = true
-vim.opt.guifont = "Caskaydia Cove Nerd Font Mono:h12"
-vim.opt.fillchars = { eob = " " }
-vim.cmd.colorscheme "gruber-darker"
-require("lualine").setup()
