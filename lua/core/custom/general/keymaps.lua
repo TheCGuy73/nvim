f3d3be814d5e174e2690489bf25aa8c3518aa2f5
TheCGuy73 <112737430+TheCGuy73@users.noreply.github.com>
2025-05-08 21:42:23 +0200

    Add files via upload

--- lua/core/custom/general/keymaps.lua
+++ lua/core/custom/general/keymaps.lua
@@ -1,12 +1,12 @@
 -- Mappature Telescope con Meta + le lettere di "telescope"
 
 local telescope = require('telescope.builtin')
-local map = vim.keymap.set
 
-map('n', '<M-t>', telescope.find_files, { desc = '[?] Find files' })
-map('n', '<M-e>', telescope.buffers, { desc = '[ ] Find existing buffers' })
-map('n', '<M-l>', telescope.live_grep, { desc = '[G]rep' })
-map('n', '<M-s>', telescope.grep_string, { desc = '[S]earch current word' })
-map('n', '<M-c>', telescope.builtin, { desc = '[B]uiltin' })
-map('n', '<M-o>', telescope.lsp_document_symbols, { desc = '[O]utline' })
-map('n', '<M-p>', function() telescope.find_files({ hidden = true }) end, { desc = '[.] Find hidden files' })
+vim.keymap.set('n', '<M-t>', telescope.find_files, { desc = '[?] Find files' })
+vim.keymap.set('n', '<M-e>', telescope.buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<M-l>', telescope.live_grep, { desc = '[G]rep' })
+vim.keymap.set('n', '<M-s>', telescope.grep_string, { desc = '[S]earch current word' })
+vim.keymap.set('n', '<M-c>', telescope.builtin, { desc = '[B]uiltin' })
+vim.keymap.set('n', '<M-o>', telescope.lsp_document_symbols, { desc = '[O]utline' })
+vim.keymap.set('n', '<M-p>', function() telescope.find_files({ hidden = true }) end, { desc = '[.] Find hidden files' })
+require('core.custom.general.utils').setup_oil_keymaps()
--- lua/core/custom/general/options.lua
+++ lua/core/custom/general/options.lua
@@ -3,5 +3,8 @@ vim.opt.guicursor = "a:block"
 vim.opt.termguicolors = true
 vim.opt.guifont = "Caskaydia Cove Nerd Font Mono:h12"
 vim.opt.fillchars = { eob = " " }
-vim.cmd.colorscheme "gruber-darker"
+vim.cmd.colorscheme "vercel"
+vim.opt.background = "dark"
+vim.opt.showmode = false
+vim.opt.showtabline = 2
 require("lualine").setup()
