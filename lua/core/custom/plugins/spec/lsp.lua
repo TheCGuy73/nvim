9434279d84ff7f6006a029718c5cf7de5242c06e
unknown <thecguy73@gmail.com>
2025-05-28 22:22:05 +0200

    Normalize line endings based on .gitattributes (280525:22.22)

--- lua/core/custom/plugins/spec/lsp.lua
+++ lua/core/custom/plugins/spec/lsp.lua
@@ -1,36 +1,31 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
+  'neovim/nvim-lspconfig',
+  dependencies = { 'saghen/blink.cmp' },
 
-      -- clangd
-      lspconfig.clangd.setup({})
-
-      -- texlab
-      lspconfig.texlab.setup({
-        settings = {
-          texlab = {
-            build = {
-              args = { "-pdf", "-interaction=nonstopmode", "-synctex=1", "%f" },
-              executable = "latexmk",
-              onSave = false,
-            },
-            forwardSearch = {
-              args = { "--file-line", "%f:%l", "%p" },
-              executable = "zathura",
-            },
-            chktex = {
-              enabled = true,
-            },
-            latexindent = {
-              enabled = true,
-            },
-            auxDirectory = "build",
-            rootDirectory = "%workspaceFolder",
-          },
-        },
-      })
-    end,
+  opts = {
+    servers = {
+      lua_ls = {},
+      clangd = {},
+      texlab = {},
+    }
   },
+  config = function(_, opts)
+    local lspconfig = require('lspconfig')
+    for server, config in pairs(opts.servers) do
+      config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+      lspconfig[server].setup(config)
+    end
+  end
+
+  -- example calling setup directly for each LSP
+  --[[
+  config = function()
+    local capabilities = require('blink.cmp').get_lsp_capabilities()
+    local lspconfig = require('lspconfig')
+
+    lspconfig['lua_ls'].setup({ capabilities = capabilities })
+    lspconfig['clangd'].setup({ capabilities = capabilities })
+    lspconfig['texlab'].setup({ capabilities = capabilities })
+  end
+  ]]
 }
