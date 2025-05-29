485cf876946c6ff57f749dd6977707b20f53fb87
unknown <thecguy73@gmail.com>
2025-05-29 22:15:55 +0200

    Using telescope as file explorer

--- lua/core/custom/plugins/spec/telescope.lua
+++ lua/core/custom/plugins/spec/telescope.lua
@@ -1,5 +1,23 @@
-return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
--- or                              , branch = '0.1.x',
-      dependencies = { 'nvim-lua/plenary.nvim' }
+return -- Using lazy.nvim
+{
+  'nvim-telescope/telescope.nvim', tag = '0.1.8',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    -- Required for file_browser
+    { 'nvim-telescope/telescope-file-browser.nvim' }
+  },
+  config = function()
+    local telescope = require("telescope")
+    telescope.setup {
+      extensions = {
+        file_browser = {
+          -- Configuration options for file_browser here (optional)
+          -- For example, to show hidden files:
+          -- hidden = true,
+        },
+      },
+    }
+    -- Load the extension
+   
+  end
 }
