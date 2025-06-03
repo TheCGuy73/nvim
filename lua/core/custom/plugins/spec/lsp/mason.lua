38cb6a586e58dc6ebfb774ce05b985d989951252
unknown <thecguy73@gmail.com>
2025-06-03 19:59:54 +0200

    regular update

--- lua/core/custom/plugins/spec/lsp/mason.lua
+++ lua/core/custom/plugins/spec/lsp/mason.lua
@@ -8,18 +8,34 @@ return {
   config = function()
     require("mason").setup()
     require("mason-lspconfig").setup({
-      ensure_installed = { "clangd", "texlab" }, -- aggiungi qui i server che vuoi
+      ensure_installed = { "clangd", "texlab" },
       automatic_installation = true,
     })
 
     local lspconfig = require("lspconfig")
     local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
     local mason_lspconfig = require("mason-lspconfig")
+
     for _, server_name in ipairs(mason_lspconfig.get_installed_servers()) do
-      lspconfig[server_name].setup({
-        capabilities = capabilities,
-      })
+      if server_name == "clangd" then
+        local compile_commands = vim.fn.getcwd() .. "/compile_commands.json"
+        local cmd = { "clangd" }
+        if vim.fn.filereadable(compile_commands) == 1 then
+          table.insert(cmd, "--compile-commands-dir=" .. vim.fn.getcwd())
+        else
+          vim.schedule(function()
+            vim.notify("clangd: compile_commands.json non trovato nella directory corrente", vim.log.levels.WARN)
+          end)
+        end
+        lspconfig.clangd.setup({
+          capabilities = capabilities,
+          cmd = cmd,
+        })
+      else
+        lspconfig[server_name].setup({
+          capabilities = capabilities,
+        })
+      end
     end
   end,
 }
