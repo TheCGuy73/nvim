666051be6f22f984f00ae326ccd041ab6f2630f9
unknown <thecguy73@gmail.com>
2025-06-08 16:38:12 +0200

    COMMIT: new theme!, VERSION: [20250608.163812], ORIGIN: origin, BRANCH: main

--- lua/core/custom/general/options.lua
+++ lua/core/custom/general/options.lua
@@ -6,12 +6,11 @@ if vim.g.neovide then
     vim.g.neovide_font = "Caskaydia Cove Nerd Font Mono:h12"
 end
 vim.opt.fillchars = { eob = " " }
-vim.cmd.colorscheme "gruber-darker"
-vim.opt.background = "dark"
+vim.cmd.colorscheme "ef-deuteranopia-light"
+
 vim.opt.showmode = false
 vim.opt.showtabline = 2
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+
 
 require('mini.statusline').setup()
 
