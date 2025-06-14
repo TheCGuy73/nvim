3ada8f3e1e1bcff8f5fbecb83adb47d86c8ea8d7
unknown <thecguy73@gmail.com>
2025-06-15 01:45:13 +0200

    .

--- lua/core/custom/general/options.lua
+++ lua/core/custom/general/options.lua
@@ -6,7 +6,7 @@ if vim.g.neovide then
     vim.g.neovide_font = "Caskaydia Cove Nerd Font Mono:h12"
 end
 vim.opt.fillchars = { eob = " " }
-vim.cmd.colorscheme "cyberdream"
+vim.cmd.colorscheme "neofusion"
 
 vim.opt.showmode = false
 vim.opt.showtabline = 2
